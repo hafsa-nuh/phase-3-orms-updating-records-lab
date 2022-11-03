cc1f5d9026cc616cf98f670b8c1b84b7ae7976aa
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-11-03 19:28:30 +0300

    commit tree 6d30fcbeb8cbfd7bdf7e567f7e5209ab30951529

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,52 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  attr_accessor :name, :grade, :id
+  def initialize(name, grade, id: nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name,grade) VALUE(?,?)
+    SQL
+    DB[:conn].execute(sql, self.name,self.grade)
+    self.id = DB[:conn].execute("SELECT last-insert_rowid() FROM students")[0][0]
+  end
+  def self.create(name:, grade:)
+    student = Student.new(name: name, grade: grade)
+    student.save
+  end
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], grade: row[2])
+  end
+  def self.find_by_name
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
 
-
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
